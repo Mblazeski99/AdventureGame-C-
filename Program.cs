82f36d53b4e7ffe1eace8beb06d031a83877153d
dblazeski96 <dblazeski96@gmail.com>
2019-05-15 00:27:05 +0200

    Add Http Data Service

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Entities;
 using Services;
 using System.IO;
+using AdventureGame.Services;
 
 namespace AdventureGame
 {
@@ -13,15 +14,17 @@ namespace AdventureGame
     {
         static void Main(string[] args)
         {
-            string eventsPath = @"C:\Users\marko\Desktop\MyApps\VisualStudio\AdvancedC#\AdventureGame-C-\Services\events.json";
-            string serializedEvents;
-            using (StreamReader sr = new StreamReader(eventsPath))
-            {
-                serializedEvents = sr.ReadToEnd();
-            }
+            //string eventsPath = @"C:\Users\marko\Desktop\MyApps\VisualStudio\AdvancedC#\AdventureGame-C-\Services\events.json";
+            //string serializedEvents;
+            //using (StreamReader sr = new StreamReader(eventsPath))
+            //{
+            //    serializedEvents = sr.ReadToEnd();
+            //}
 
-            GameService gs = new GameService(serializedEvents);
-            gs.StartGame();
+            //GameService gs = new GameService(serializedEvents);
+            //gs.StartGame();
+            List<Event> data = DataService.GetData<Event>(@"https://raw.githubusercontent.com/Mblazeski99/AdventureGame-C-/master/Services/events.json").Result;
+            Console.WriteLine(data[0].Title);
                         
             Console.ReadKey();
         }
---
+++ Services/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Entities;
+
+namespace AdventureGame.Services
+{
+    public static class DataService
+    {
+        public async static Task<List<ResponseData>> GetData<ResponseData>(string url)
+        {
+            string data;
+            WebResponse res = await WebRequest.CreateHttp(url).GetResponseAsync();
+            using(StreamReader resReader = new StreamReader(res.GetResponseStream()))
+            {
+                data = resReader.ReadToEnd();
+            }
+            return JsonConvert.DeserializeObject<List<ResponseData>>(data);
+        }
+    }
+}
